fb6dfcbae79b7d1bf6f6e93912f6d991766b880f
Goatja <jorge.arana528@gmail.com>
2025-11-25 21:47:26 -0600

    Add products routes at api.php using protected routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -13,3 +14,13 @@ Route::get('/user', function (Request $request) {
 Route::post('/register',[UserController::class,'register']);
 Route::post('/login',[UserController::class,'login']);
 Route::post('/logout',[UserController::class,'logout'])->middleware('auth:sanctum');
+
+
+//Rutas protegidas
+Route::middleware('auth:sanctum')->group(function() {
+
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
